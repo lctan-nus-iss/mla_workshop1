96860c70aedd10830f7b4a3bcee3fc7c5e37d43f
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-06 15:11:01 +0800

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,11 @@
+## Exercise 9
+'''
+Create a list with the following values 5, 6, 4, 7, 10 <br>
+Answer the following questions. <br>
+a) Sort the array. (Use the sort method) <br>
+b) Access the 4th element of the sorted array. <br>
+c) Reverse the order of the array. (Use the reverse method) <br>
+d) Remove the value 6 from the array. <br>
+e) Delete the third element. <br>
+f) Delete the first two elements <br>
+'''
