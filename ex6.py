d7254a1a66ebac9b8969b2f92222a3a3ed080e7a
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-06 15:06:28 +0800

    Create Triangle class with angle checking method
    Implemented Triangle class with angle validation.

---
+++ ex6.py
@@ -0,0 +1,10 @@
+## Exercise 6
+'''
+Follow the steps:
+
+1. Create a class, Triangle. Its __init__() method should take self, angle1, angle2, and angle3 as arguments. Make sure to set these appropriately in the body of the __init__()method.
+2. Create a variable named number_of_sides and set it equal to 3.
+3. Create a method named check_angles. The sum of a triangle's three angles is It should return True if the sum of self.angle1, self.angle2, and self.angle3 is equal 180, and False otherwise.
+4. Create a variable named my_triangle and set it equal to a new instance of your Triangle class. Pass it three angles that sum to 180 (e.g. 90, 30, 60).
+5. Print out my_triangle.number_of_sides and print out my_triangle.check_angles().
+'''
