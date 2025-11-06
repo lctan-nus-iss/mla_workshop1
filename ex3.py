f5b38ef97aea79d4b415ead990f45a4ea99852c9
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-06 14:43:08 +0800

    Add shut_down function for shutdown logic
    Implement shut_down function with conditional responses.

---
+++ ex3.py
@@ -0,0 +1,12 @@
+## Exercise 3
+'''
+Write a shutting down program:
+
+First, def a function, shut_down, that takes one argument s. 
+
+Then, if the shut_down function receives an s equal to "yes", it should return "Shutting down" 
+
+Alternatively, elif s is equal to "no", then the function should return "Shutdown aborted". 
+
+Finally, if shut_down gets anything other than those inputs, the function should return "Sorry".
+'''
