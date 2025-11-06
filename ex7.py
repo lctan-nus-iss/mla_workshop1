9bfec7c7c2900b62baaa53fd06bf1acaf9404f08
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-06 15:07:46 +0800

    Implement list counting and dictionary storage
    Define a list of integers and count occurrences.

---
+++ ex7.py
@@ -0,0 +1,8 @@
+## Exercise 7
+
+# Define a list and populate it with integer numbers from 1-10.
+
+# Write a loop that count the content of the list and store the information in a dictionary
+
+# For example, if the list is \[1, 3, 3, 1, 2] then the dictionary should contain
+# { 1: 2, 2: 1, 3: 2}
