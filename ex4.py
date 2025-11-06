4e5ff986e0f4b925b87fc53defd49637029adef4
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-06 14:49:21 +0800

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,3 @@
+## Exercise 4
+
+# Write a function that accepts a sentence and capitalize the first letter of each words in the string. You can use split() method to split the sentence into its words
