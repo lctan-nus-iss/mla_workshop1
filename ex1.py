d7effa534b402f4e167d646b017a5a59489acf2c
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-06 14:31:42 +0800

    Add gross pay calculation program
    Implement a program to calculate gross pay based on hours and hourly rate.

---
+++ ex1.py
@@ -0,0 +1,1 @@
+# Write a program to prompt the user for hours and rate per hour to compute gross pay.Take into account that the factory gives the employee 1.5 times the hourly rate for hours worked above 40 hours.
